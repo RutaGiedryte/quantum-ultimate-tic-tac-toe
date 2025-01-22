6384cea86dfd29997ccdd490ff513eff673e74a4
Nils Marten Mikk <nilsmikk@gmail.com>
2025-01-22 12:52:56 +0100

    Create slider for angle with pi/4 step

--- src/gui/widgets/angle_selection.py
+++ src/gui/widgets/angle_selection.py
@@ -1,20 +1,26 @@
-from tkinter import StringVar, Widget, ttk
+from tkinter import StringVar, Widget, ttk, DoubleVar
+import math
 
 
 class AngleSelection(ttk.Frame):
     """Widget for selecting the rotation angle when rotating a qubit."""
 
-    def __init__(self, parent: Widget, textvariable: StringVar, callback, **kw) -> None:
+    def __init__(self, parent: Widget, angle_var: DoubleVar, callback, **kw) -> None:
         """Create angle selection widget.
 
         Args:
             parent: parent widget
-            textvariable: variable for the angle
-            callback: function to call when submitting the angle
+            angle_var: variable for the angle
+            callback: function to call when changing the angle
             kw: keyword arguments for `ttk.Frame`
         """
 
         super().__init__(parent, **kw)
+
+        self._angle_var = angle_var
+
+        # values from -pi with pi with pi/4 step
+        self._allowed_values = [math.pi * (i / 4) if i else 0 for i in range(-4, 5)]
 
         # create info label
         self._text = StringVar()
@@ -22,16 +28,14 @@ class AngleSelection(ttk.Frame):
             self,
             textvariable=self._text,
             padding="0 0 0 5",
-        ).grid(row=0, column=0, columnspan=2)
+        ).grid(row=0, column=0)
 
-        # entry widget
-        self._angle = textvariable
-        self._entry = ttk.Entry(self, textvariable=self._angle)
-        self._entry.grid(row=1, column=0)
+        # create slider
 
-        # submit widget
-        self._button = ttk.Button(self, text="enter", command=callback)
-        self._button.grid(row=1, column=1)
+        self._slider = ttk.Scale(
+            self, length=300, command=self._change_angle, variable=angle_var
+        )
+        self._slider.grid(row=1, column=0)
 
         self._callback = callback
 
@@ -45,17 +49,33 @@ class AngleSelection(ttk.Frame):
         self._text.set(message)
 
     def disable(self) -> None:
-        """Disable the button and enrty."""
+        """Disable the angle slider."""
 
-        self._entry["state"] = "disabled"
-        self._button["state"] = "disabled"
+        self._slider.configure(state="disabled")
 
-    def enable(self) -> None:
-        """Enable the button and entry.
+    def enable(self, max: float) -> None:
+        """Enable the angle slider.
 
-        Clears the angle entry box. Focuses on angle entry box.
+        Sets the maximum value, and sets the current value to 0.
+
+        Args:
+            max: max. angle
         """
-        self._angle.set("")
-        self._entry["state"] = "normal"
-        self._button["state"] = "normal"
-        self._entry.focus()
+        self._slider.configure(from_=-max, to=max, state="normal")
+        self._angle_var.set(0)
+
+    def _change_angle(self, angle) -> None:
+        """Callback function for changing the angle on the slider.
+
+        Args:
+            angle: new angle
+        """
+
+        angle = float(angle)
+
+        # round to allowed value
+        if angle not in self._allowed_values:
+            closest = min(self._allowed_values, key=lambda val: abs(val - angle))
+            self._slider.set(closest)
+
+        self._callback()
